42616f2a832b481f656c7687a58d8ba5d17cebe8
ornob007 <mlheartbeat007@gmailcom>
2019-07-04 20:23:21 +0600

    added

---
+++ photo_blog_oop/includes/config.php
@@ -0,0 +1,10 @@
+<?php 
+	// database constant
+
+defined('DB_SERVER') ? null : define('DB_SERVER','localhost');
+defined('DB_USER') ? null : define('DB_SERVER','root');
+defined('DB_PASS') ? null : define('DB_SERVER','');
+defined('DB_NAME') ? null : define('DB_SERVER','photo_gallery');
+
+
+?>
