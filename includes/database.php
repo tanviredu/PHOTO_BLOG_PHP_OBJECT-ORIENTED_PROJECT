f3ac08c5043b82e9a303b81f89f8dd57f1add192
ornob007 <mlheartbeat007@gmailcom>
2019-07-05 11:16:01 +0600

    initial commit

--- includes/database.php
+++ includes/database.php
@@ -5,11 +5,15 @@
         private $connection;
 
         function __construct(){
+            $this->open_connection();
 
         }
 
         public function open_connection(){
-
+            $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+            if(!$connection){
+                die("Connection failed");
+            }
         }
 
         public function close_connection(){
