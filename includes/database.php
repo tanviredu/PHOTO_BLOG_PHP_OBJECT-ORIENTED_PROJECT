3950c68e655aff27e4954a04a8dffef9a8c80d0d
ornob007 <mlheartbeat007@gmailcom>
2019-07-05 11:49:49 +0600

    initial commit

--- includes/database.php
+++ includes/database.php
@@ -10,17 +10,47 @@
         }
 
         public function open_connection(){
-            $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-            if(!$connection){
+            $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+            if(!$this->connection){
                 die("Connection failed");
             }
         }
 
         public function close_connection(){
+            //closing connection
+            if(isset($this->connection)){
+                mysqli_close($this->connection);
+                unset($this->connection);
+            }
 
         }
+
+
+        public function confirm_query($result){
+            if(!result){
+                die("Database query failed");
+            }
+        }
+
+        public function query($sql){
+            $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+            $result = mysqli_query($sql,$$this->connection);
+            $this->confirm_query($result);
+            return $result;
+        }
+
+        public function mysql_prep($value){
+            // functionality will be added later;
+            return $value;
+        }
+
+
+
+
+
     }
 
 $databse = new MySqlDatabse();
+$databse->close_connection();
 
 ?>
