b0b3c915837262dbe75555d66d96627f7a545b05
ornob007 <mlheartbeat007@gmailcom>
2019-07-05 11:08:56 +0600

    initial commit

---
+++ includes/database.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('config.php');
+
+    class MySqlDatabse{
+        private $connection;
+
+        function __construct(){
+
+        }
+
+        public function open_connection(){
+
+        }
+
+        public function close_connection(){
+
+        }
+    }
+
+$databse = new MySqlDatabse();
+
+?>
