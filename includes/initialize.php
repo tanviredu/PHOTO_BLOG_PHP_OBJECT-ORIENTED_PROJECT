dd3bf4ede62519b97922b715c389cb0ce0e6b3c8
ornob007 <mlheartbeat007@gmailcom>
2019-07-08 19:30:32 +0600

    added

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,7 +1,23 @@
 <?php
-require_once('config.php');
-require_once('database.php');
-require_once('user.php');
-require_once('session.php');
-require_once('functions.php');
+
+/**
+ * setting the absolute file system  path for the
+ *  root root directory the library path
+ */
+
+defined('DS') ? null : define('DS',DIRECTORY_SEPARATOR);
+    defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'opt'.DS.'lampp'.DS.'htdocs'.DS.'PHOTO_BLOG_PHP_OBJECT-ORIENTED_PROJECT');
+    defined('LIB_PATH') ? null : define('LIB_PATH',SITE_ROOT.DS.'includes');
+
+    require_once(LIB_PATH.DS.'config.php');
+    require_once(LIB_PATH.DS.'database.php');
+
+    /**
+     *if you make a parent class you must import if before the child class
+     * called thats why database_object class first then the user class
+     * */
+    require_once(LIB_PATH.DS.'database_object.php');
+    require_once(LIB_PATH.DS.'user.php');
+    require_once(LIB_PATH.DS.'session.php');
+    require_once(LIB_PATH.DS.'functions.php');
 ?>
--- includes/user.php
+++ includes/user.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('database.php');
+    require_once(LIB_PATH.DS.'database.php');
 
 
 
@@ -18,7 +18,7 @@
  *
  * we use the self static method find_by_sql()
  *
- * as long as you use fetch_array() you will get an array instted of user object
+ * as long as you use fetch_array() you will get an array insted of user object
  * so have to change that
  *
  * now we make a instantiate method so we pass the data with sql and the User class
@@ -28,8 +28,13 @@
 
  */
 
-    class User{
+    class User extends DatabaseObject{
 
+        /**
+         * importing from parent class thats why we have to write the table name
+         * it DatabaseObject will be imported multiple time by different claas
+         */
+        protected static $table_name = "users";
         public $id;
         public $username;
         public $password;
@@ -37,54 +42,9 @@
         public $last_name;
 
 
-        public static function find_all(){
-            global $database;
-            //$result_set = $database->query("SELECT * FROM users");
-            //return $result_set;
-            return self::find_by_sql("SELECT * FROM users");
-            // now it automatically return object array
-        }
-
-        public static function find_by_id($id){
-            global $database;
-            //$result_set = $database->query("SELECT * FROM users WHERE id='$id'");
-
-              $result_array = self::find_by_sql("SELECT * FROM users WHERE id={$id} LIMIT 1");
-            /**$found = $database->fetch_array($result_set);
-                *return $found;
-             * WE CANT USE IT NOW CAUSE ITS RETURING OBJECT ARRAY NOW
-             * */
-             return !empty($result_array) ? array_shift($result_array) : false;
-
-            // it means if it is not empty fetch the first element and if empty send false
-
-        }
-
-        public static function find_by_sql($sql=""){
-
-            /**
-             * we fetch data from the sql with query
-             * then we declare an empty object array
-             * then we iterate through the result_set and every row
-             * is a record of the user object means every row of this result_set
-             * has all the attribute info so we instantiate the object with every row
-             * and then store this ionstantiated object in object array
-             * */
 
 
-
-
-            global $database;
-            $result_set = $database->query($sql);
-            $object_array = array();
-            while($row = $database->fetch_array($result_set)){
-                $object_array[] = self::instantiate($row);
-            }
-
-            //return $result_set;
-            return $object_array;
-        }
-
+        // these are are the unique classs
         public function full_name(){
             if(isset($this->first_name) && isset($this->last_name)){
                 return $this->first_name . " " .$this->last_name;
@@ -93,45 +53,9 @@
             }
         }
 
-
-        private function has_attribute($attribute){
-            // first find all the attribute and store it
-            $object_vars = get_object_vars($this); //this instant
-            return array_key_exists($attribute,$object_vars);
-        }
-
-        private static function  instantiate($record){
-           $object = new self;
-            /**
-             * BASIC WAY OF INSTANTIATE THE OBJECT
-            $object->id = $record['id'];
-            $object->username = $record['username'];
-            $object->password = $record['password'];
-            $object->first_name = $record['first_name'];
-            $object->last_name = $record['last_name'];
-            return $object;
-
-
-            // this is more dynamic approach
-            //check weather the object has a attribute
-            // and if it is then add the value of that
-            // attribute
-            // iterte through a dictionary
-             **/
-            foreach($record as $attribute=> $value){
-                if($object->has_attribute($attribute)){
-                    $object->$attribute=$value;
-                }
-            }
-
-
-            return $object;
-        }
-
-
         public static function authenticate($username='',$password=''){
 
-            $result_array=self::find_by_sql("SELECT * FROM users WHERE username='$username' AND password='$password' LIMIT 1");
+            $result_array=self::find_by_sql("SELECT * FROM ".self::$table_name." WHERE username='$username' AND password='$password' LIMIT 1");
             /**$found = $database->fetch_array($result_set);
              *return $found;
              * WE CANT USE IT NOW CAUSE ITS RETURING OBJECT ARRAY NOW
