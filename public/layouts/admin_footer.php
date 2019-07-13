3cc2f371fdea699f635c7974893e9cacca56b885
ornob007 <mlheartbeat007@gmailcom>
2019-07-13 21:08:33 +0600

    deleted

--- public/layouts/admin_footer.php
+++
@@ -1,5 +0,0 @@
-    </div>
-    <div id="footer">Copyright <?php echo date("d-m-Y", time()); ?>, Tanvir Rahman</div>
-  </body>
-</html>
-<?php if(isset($database)) { $database->close_connection(); } ?>
--- public/layouts/footer.php
+++
@@ -1,5 +0,0 @@
-    </div>
-    <div id="footer">Copyright <?php echo date("Y", time()); ?>, Tanvir Rahman</div>
-  </body>
-</html>
-<?php if(isset($database)) { $database->close_connection(); } ?>
